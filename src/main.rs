1d481d3c57aae40dae8488120a434d3bdc597907
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-06-22 19:40:36 -0700

    Add output to README

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 /// Feedback welcome.
 ///
 /// This repo is based on the artice about string handling here:
-/// https://mortoray.com/2013/11/27/the-string-type-is-broken/
+///
+/// * https://mortoray.com/2013/11/27/the-string-type-is-broken/
 ///
 ///
 /// This code is tested on Rust 1.5.3:
@@ -12,6 +13,25 @@
 /// ```sh
 /// $ rustc --version                                                                     
 /// rustc 1.53.0
+/// ```
+///
+/// To use this code, download it and run it as usual for Rust:
+///
+/// ```sh
+/// cargo run
+/// ```
+///
+/// Output:
+///
+/// ```sh
+/// Does it print correctly? noël
+/// What is the character length? 4
+/// What is the reverse? lëon
+/// What are the first three characters? noë
+/// Does it print correctly? 😸😾
+/// What is the reverse? 😾😸
+/// What is the character length? 2
+/// What is the substring after the first character? 😾
 /// ```
 ///
 /// Tracking:
